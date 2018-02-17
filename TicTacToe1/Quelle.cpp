a71acedb026691e939ea3f3affe38ee1c1dc1539
brabh <hans@brabenetz.com>
2018-02-17 16:09:22 -0500

    ok

---
+++ TicTacToe1/Quelle.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+	int nGame, NGame = 1000;
+
+	for (nGame = 0; nGame < NGame; ++nGame) {
+
+
+
+
+
+	} // 	for (nGame = 0; nGame < NGame; ++nGame) {
+
+
+
+
+
+	getchar();
+	return 0;
+}
+
+
+
+
+
+
+
+
